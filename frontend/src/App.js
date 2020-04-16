65d013235b953cf6358154e09defa911782fa769
victorgcampanella <vgcgalvao@outlook.com>
2020-04-16 12:08:26 -0300

    propriedades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,23 @@ import React from 'react'
 import Header from './components/Header'
 
 function App() {
-  return <Header />
+  return (
+    <>
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projetos</li>
+        </ul>
+      </Header>
+      <Header title="Projets">
+        <ul>
+          <li>Homepage</li>
+          <li>Projetos</li>
+          <li>Login</li>
+        </ul>
+      </Header>
+    </>
+  )
 }
 
 export default App
