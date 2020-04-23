f95a142c441ed4f79d73677bef867879af656356
victorgcampanella <vgcgalvao@outlook.com>
2020-04-23 17:54:57 -0300

    organizando o react native

---
+++ mobile/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <Text >Hello World</Text>
+  );
+}
