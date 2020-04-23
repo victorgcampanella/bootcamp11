2a02b4788de8d86f0ad79fba44803c7dedb7d83c
victorgcampanella <vgcgalvao@outlook.com>
2020-04-23 14:38:25 -0300

    adicionando um novo projeto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,9 +14,17 @@ function App() {
     })
   }, [])
 
-  function handleAddProject() {
+  async function handleAddProject() {
     // projects.push(`Novo projeto ${Date.now()}`)
-    setProjects([...projects, `Novo projeto ${Date.now()}`])
+    // setProjects([...projects, `Novo projeto ${Date.now()}`])
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Diego Fernandes"
+    })
+
+    const project = response.data
+
+    setProjects([...projects, project])
   }
 
   return (
