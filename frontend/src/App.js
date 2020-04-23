b9fd863ad565d81e94b8140f6c535de81de5f306
victorgcampanella <vgcgalvao@outlook.com>
2020-04-23 13:49:50 -0300

    listando dados do backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import api from './services/api'
 
 import './App.css'
 
 import Header from './components/Header'
-import backgroundImage from './assets/background.jpg'
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-end Web'])
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
 
   function handleAddProject() {
     // projects.push(`Novo projeto ${Date.now()}`)
     setProjects([...projects, `Novo projeto ${Date.now()}`])
-    console.log(projects)
   }
 
   return (
     <>
       <Header title="Projects"/>
 
-      <img src={backgroundImage} width={300}/>
-
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
