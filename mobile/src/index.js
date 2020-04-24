cecd950320888e7c5f9bd0174d7fc15bd91bd7b4
victorgcampanella <vgcgalvao@outlook.com>
2020-04-24 16:08:06 -0300

    aplicando css no react native

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
-
-// import { Container } from './styles';
+import { View, Text, StatusBar, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <Text >Hello World</Text>
+    <>
+    <StatusBar barStyle="light-content" backgroundColor= "#7159c1"/>
+    <View style={styles.view}>
+      <Text style={styles.text}>Hello Rocketseat</Text>
+    </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#7159c1"
+  },
+
+  text: {
+    fontSize: 32,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 20
+  }
+})
