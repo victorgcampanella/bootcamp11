641e035ae316f16c715a57954625d1512524c38c
victorgcampanella <vgcgalvao@outlook.com>
2020-04-30 17:08:01 -0300

    tipando hello world

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,18 @@
 import {Request, Response} from 'express'
+import createUser from './services/CreateUser'
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'victor@gmail.com',
+    password: '123456',
+    techs: [
+      'NodeJS', 
+      'React',
+      {title: 'JavaScript', experience: 100},
+    ]
+  })
+
+  console.log(user.email)
+
   return response.json({message : 'Hello World'})
 }
