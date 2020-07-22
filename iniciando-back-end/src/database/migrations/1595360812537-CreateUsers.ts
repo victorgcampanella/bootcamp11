4a2101439388f2385f0646385fa9d6aeb772b568
victorgcampanella <vgcgalvao@outlook.com>
2020-07-22 12:57:09 -0300

    relacionamento nos models

--- iniciando-back-end/src/database/migrations/1595360812537-CreateUsers.ts
+++ iniciando-back-end/src/database/migrations/1595360812537-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1595360812537 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
