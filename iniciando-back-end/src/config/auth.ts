4c130b2e3f48f8ea47cc1107f11b4eaa1d822ebc
victorgcampanella <vgcgalvao@outlook.com>
2020-07-23 14:52:00 -0300

    gerando jwt e rotas autenticadas

---
+++ iniciando-back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'd47083ced5be37e52f972f3b2c2f8cd9',
+    expiresIn: '1d',
+  },
+};
--- iniciando-back-end/src/services/AuthenticateUserService.ts
+++ iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -1,5 +1,9 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +13,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -27,8 +32,17 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const { secret, expiresIn } = authConfig.jwt;
+
+    // bootcamp11jsonwebtoken
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
     return {
       user,
+      token,
     };
   }
 }
